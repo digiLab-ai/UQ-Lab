8c740d996a8a51e267c1fc93159173a77ea52268
Freddy Wordingham <fjmw201@exeter.ac.uk>
2022-07-08 00:23:23 +0100

    Added login page

--- app/api/database.py
+++ app/api/database.py
@@ -1,8 +1,20 @@
+import json
 from pathlib import Path
 import glob
 import os
 
 from . import settings
+
+
+def validate_credentials(username, password):
+    """
+    Check if a login is valid
+    """
+
+    return (
+        json.load(os.path.join(settings.RESOURCES_DIR, "users.json")).get(username)
+        == password
+    )
 
 
 def get_campaign_names():
--- app/api/main.py
+++ app/api/main.py
@@ -10,7 +10,15 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/login", response_class=HTMLResponse)
+async def login(request: Request):
+    return settings.templates.TemplateResponse(
+        "pages/login.html",
+        {"request": request},
+    )
+
+
+@app.get("/index", response_class=HTMLResponse)
 async def index(request: Request):
     return settings.templates.TemplateResponse(
         "pages/index.html",
