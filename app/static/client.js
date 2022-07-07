b754670c4af7e54286f864a02db9747b138bb79e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2022-07-08 00:40:35 +0100

    Added login page

--- app/static/client.js
+++ app/static/client.js
@@ -3,4 +3,13 @@ class Client {
     static go_to_homepage() {
         window.location.href = "/";
     }
+
+    // == User ==
+    static validate_credentials(username, password, success) {
+        $.ajax({
+            url: "/validate_credentials/" + username + "/" + password,
+            type: "GET",
+            success: success,
+        });
+    }
 }
