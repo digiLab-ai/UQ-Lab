48e0c641acaf37309d584ba9c9a878d9aef7f487
Freddy Wordingham <fjmw201@exeter.ac.uk>
2022-07-11 20:13:59 +0100

    Fixed page pushing

--- app/static/client.js
+++ app/static/client.js
@@ -1,6 +1,10 @@
 class Client {
     // == Navigation ==
     static go_to_overview() {
-        window.location.href = "/overview";
+        window.location.href = "/";
+    }
+
+    static go_to_login() {
+        window.location.href = "/login";
     }
 }
